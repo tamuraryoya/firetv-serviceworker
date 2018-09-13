cbee18e0a9ea1e398ce9efe3ecc4ff9d2fc13cec
tamuraryoya <tamuraryoya@team-lab.com>
2018-09-13 22:57:14 +0900

    🚀 update released.

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,9 @@
 self.addEventListener('install', () => {
-  console.log('installed.');
+  console.log('installed.', caches);
+});
+
+self.addEventListener('activate', () => {
+  console.log('V1 now ready to handle fetches!');
 });
 
 self.addEventListener('fetch', (e) => {
